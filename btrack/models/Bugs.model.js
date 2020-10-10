1360f07759721bcb9c9c9cbb7b32ddb841c3dfde
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-10-10 23:05:09 +0200

    add bug details + add solution + refacto

--- btrack/models/Bugs.model.js
+++ btrack/models/Bugs.model.js
@@ -1,19 +1,30 @@
 const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
+const SeveritySchema = new mongoose.Schema({
+    value: { type: String },
+    updated: { type: Boolean },
+});
 const serviceSchema = new Schema(
     {
-        title: {type: String},
+        title: { type: String },
         rapporter: { type: Schema.Types.ObjectId, ref: 'User' },
-        description: {type: String},
+        description: { type: String },
+        severty: { type: String },
+        rapportedAt: { type: Date },
         solutions: [{
             user_id: { type: Schema.Types.ObjectId, ref: 'User' },
-            solution: {type: String},
+            solution: { type: String },
+            status: { type: String },
+            severity: { type: SeveritySchema, default: {value: 'Medium', updated: false} },
+            date: { type: Date }
         }],
         services: [{ type: Schema.Types.ObjectId, ref: 'Service' }],
-        status: {type: String},
+        status: { type: String },
     }
 );
+
+
 
 module.exports = model("Bug", serviceSchema);
 
--- btrack/routes/bugs.js
+++ btrack/routes/bugs.js
@@ -1,68 +1,106 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Bug= require('../models/Bugs.model.js');
+const moment = require('moment')
+const Bug = require('../models/Bugs.model.js');
 const User = require('../models/User.model.js');
 const Service = require("../models/Services.model.js");
 const router = express.Router();
+const { body, check, validationResult } = require('express-validator');
 
-// route vers la formulaire de de mise à jours du bug:
-router.get('/:id/editBug', (req, res, next) =>{
+
+
+
+
+// details du bug
+router.get('/:id/details', (req, res, next) => {
   let user = req.session.user;
   if (req.session.user) {
-    Service.find({})
-      .lean()
-      .then(servicesFromDB => {
-        Bug.findById(req.params.id)
-            .lean()
-            .populate('rapporter')
-            .populate('solutions.user_id')
-            .then(bug =>res.render('account/editBug', {bug, servicesFromDB, user}))
-            .catch(err => next(err))
-      })
-      .catch(err => next(err))
-    }else {
-      res.redirect('/login')
-    }        
-})
-// route de traitement du formulaire de bug: ajout d'instance
-router.post("/:id", (req, res, next) => {
-  let user = req.session.user;
-  if (req.session.user) {  
-    const {status, services, solution} = req.body;
-    Bug.findByIdAndUpdate(req.params.id, 
-      {services, status,
-        $push:{solutions: [{ user_id: req.session.user._id, solution }]}
-    }, {new: true})
-    .lean()
-    .populate('services')
-    .populate('rapporter')
-    .populate('solutions.user_id')
-    .then(bug => {res.render('account/bug-details', {bug, user})
-    }).catch(err => next(err))
-  }else {
-    res.redirect('/login')
-  } 
-})
-
-
-router.get('/:bugid', (req, res, next) =>{
-    let id = req.params.bugid;
-    if (req.session.user) {
-      Bug.findOne({_id: id})
-      .lean()
-      .populate('services')
+    Bug.findById(req.params.id)
       .populate('rapporter')
-      .populate('user_id')
-      .then((bug) => {
-          res.render('account/bug-details', {
-              bug
-          })
+      .populate('solutions.user_id')
+      .populate('services')
+      .lean()
+      .then(result => {
+        console.log('result',result)
+        const sortedSolutions = result.solutions.sort((s1, s2) => s2.date - s1.date) 
+        console.log('res',res)
+        const solutions = sortedSolutions.map(s => {
+          const solution = {
+            ...s, date: {
+              rapportDay: moment(s.date).format('ll'),
+              rapportTime: moment(s.date).format('LT'),
+            }
+          }
+          return solution
+        })
+        const bug = {
+          ...result, date: {
+            rapportDay: moment(result.rapportedAt).format('ll'),
+            rapportTime: moment(result.rapportedAt).format('LT')
+          },
+          solutions
+        }
+        console.log('bug', bug)
+        res.render('account/bug-details', { bug, user })
       })
+
       .catch(err => next(err))
-    }else {
-        res.redirect('/login')
-    }   
+  } else {
+    res.redirect('/login')
+  }
+})
+
+router.post("/:id/solution",
+  check('solution')
+    .notEmpty().withMessage('A solution must not be empty')
+    .isLength({ max: 3000 }).withMessage('A solution must exceed 3000 chars long.')
+  , (req, res, next) => {
+    const user = req.session.user;
+    const bugId = req.params.id;
+    if (user) {
+      const result = validationResult(req);
+      if (result.isEmpty()) {
+        const { status, severity, solution } = req.body;
+        Bug.findByIdAndUpdate(bugId,
+          {
+            status, severity,
+            $push: { solutions: [{ user_id: user._id, solution, status, severity: {value: severity, updated: true}, date: Date.now() }] }
+          }, { new: true })
+          .then(bug => {
+            res.redirect('/bugs/' + req.params.id + '/details')
+          }).catch(err => next(err))
+      } else {
+        Bug.findById(bugId)
+          .lean()
+          .then(bug => {
+            res.render('account/bug-details', { bug, user, errors: esult.errors.map(e => e.msg) })
+          })
+      }
+    } else {
+      res.redirect('/login')
+    }
   })
+
+
+
+
+// router.get('/:id', (req, res, next) => {
+//   let bugId = req.params.id;
+//   if (req.session.user) {
+//     Bug.findOne({ _id: bugId })
+//       .lean()
+//       .populate('services')
+//       .populate('rapporter')
+//       .populate('user_id')
+//       .then((bug) => {
+//         console.log(bug)
+//         res.render('account/bug-details', {bug })
+//       })
+//       .catch(err => next(err))
+//   } else {
+//     res.redirect('/login')
+//   }
+// })
 
 
 
